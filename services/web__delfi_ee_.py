734e6b4ebe02fb92460591b6aba78f1fc9a865ad
rudolph <rudolphos@users.noreply.github.com>
2016-10-30 03:42:01 +0300

    Create web__delfi_ee_.py
    Estonian (Estonia) news site delfi.ee

---
+++ services/web__delfi_ee_.py
@@ -0,0 +1,8 @@
+refresh = 6
+version = 20161029.01
+
+urls = ['http://www.delfi.ee/',
+	'http://feeds2.feedburner.com/delfiuudised']
+regex = [r'^http?:\/\/[^\/]*delfi\.ee\/']	
+videoregex = []
+liveregex = []
